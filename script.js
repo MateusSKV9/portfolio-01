15641fd2fcd709ccd4c9342bdc078b0dd397b9cd
Mateus Santos <mateussilvanzg@gmail.com>
2024-07-23 14:38:31 -0300

    Update script

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 function menu () {
     if(window.innerWidth <= 600) {
-        itens.style.display = 'none'
+        nav.style.display = 'none'
     }
 }
 
 function mudouTam () {
     if(window.innerWidth >= 600) {
-        itens.style.display = 'block'
+        nav.style.display = 'flex'
     } else {
-        itens.style.display = 'none'
+        nav.style.display = 'none'
     }
 }
 
 function clickMenu () {
-    if(itens.style.display == 'block') {
-        itens.style.display = 'none'
+    if(nav.style.display == 'flex') {
+        nav.style.display = 'none'
     } else {
-        itens.style.display = 'block'
+        nav.style.display = 'flex'
     }
 }
+let nav = document.getElementById('itens');
+let burguer = document.getElementById('burguer');
+burguer.addEventListener('click', clickMenu);
 
 function mudarTema () {
     const html = document.documentElement;
-    html.classList.toggle("dark");
+    html.classList.toggle('dark');
 
-    const divTema = document.getElementById("switch");
+    const divTema = document.getElementById('switch');
 
-    if(divTema.classList.contains("light")) {
-        divTema.classList.remove("light");
-        divTema.classList.add("dark");
-    } else if (divTema.classList.contains("dark")) {
-        divTema.classList.remove("dark");
-        divTema.classList.add("light");
+    if(divTema.classList.contains('light')) {
+        divTema.classList.remove('light');
+        divTema.classList.add('dark');
+    } else if (divTema.classList.contains('dark')) {
+        divTema.classList.remove('dark');
+        divTema.classList.add('light');
     }
-    
 }
 let btnTema = document.getElementById("switch");
 btnTema.addEventListener("click", mudarTema);
